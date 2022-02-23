c7cf4fc116b47378422acd6489f453bb71d13b16
Julian Amaya <julian.amayad@gmail.com>
2022-02-23 10:34:45 -0500

    React Query Devtools

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Route,
 } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { HomePage } from './pages/Home';
 import { TeamsPage } from './pages/Teams';
 import { PlayersPage } from './pages/Players';
@@ -15,17 +16,18 @@ function App() {
   const queryClient = new QueryClient()
   return (
     <>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/players" element={<PlayersPage />} />
-          <Route path="/teams" element={<TeamsPage />} />
-          </>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/players" element={<PlayersPage />} />
+            <Route path="/teams" element={<TeamsPage />} />
+            </>
+          </Routes>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpem={false} position='bottom-right' />
       </QueryClientProvider>
     </>
   );
