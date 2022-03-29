de7423072f33c6f9312e7feb72d5a052ff4ca0d3
Julian Amaya <julian.amayad@gmail.com>
2022-03-29 07:15:21 -0500

    Initial Query Data

--- src/hooks/usePlayerData.js
+++ src/hooks/usePlayerData.js
@@ -1,9 +1,18 @@
 
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import axios from 'axios';
 
 export const usePlayerData = (playerId) => {
+  const queryClient = useQueryClient()
   return useQuery(['player', playerId], () => {
     return axios.get(`http://localhost:4000/players/${playerId}`)
+  }, {
+    initialData: () => {
+      console.log(queryClient.getQueryData('players'));
+      const player = queryClient.getQueryData('players')?.data?.find((player) => player.id === playerId)
+      console.log({ player });
+      if (player) return { data: player };
+      return undefined;
+    }
   })
 }
